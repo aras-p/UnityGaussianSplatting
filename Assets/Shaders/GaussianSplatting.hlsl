d8163efc9010daf4b8ab4628b156f768d6d87669
Aras Pranckevicius <aras@nesnausk.org>
2023-09-03 13:13:55 +0300

    Fix missing cov2d limit for partially off-screen splats

--- Assets/Shaders/GaussianSplatting.hlsl
+++ Assets/Shaders/GaussianSplatting.hlsl
@@ -49,6 +49,15 @@ float3 CalcCovariance2D(float3 worldPos, float3 cov3d0, float3 cov3d1)
     float4x4 viewMatrix = UNITY_MATRIX_V;
     float3 viewPos = mul(viewMatrix, float4(worldPos, 1)).xyz;
 
+    // this is needed in order for splats that are visible in view but clipped "quite a lot" to work
+    float aspect = UNITY_MATRIX_P._m00 / UNITY_MATRIX_P._m11;
+    float tanFovX = rcp(UNITY_MATRIX_P._m00);
+    float tanFovY = rcp(UNITY_MATRIX_P._m11 * aspect);
+    float limX = 1.3 * tanFovX;
+    float limY = 1.3 * tanFovY;
+    viewPos.x = clamp(viewPos.x / viewPos.z, -limX, limX) * viewPos.z;
+    viewPos.y = clamp(viewPos.y / viewPos.z, -limY, limY) * viewPos.z;
+
     float focal = _ScreenParams.x * UNITY_MATRIX_P._m00 / 3;
 
     float4x4 J = float4x4(
